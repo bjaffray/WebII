05886d2633df0e697bc570e4f94201658b4b0a21
bjaffray <jaf15001@byui.edu>
2019-05-04 23:35:00 -0600

    Updates

--- web/navBar.php
+++ web/navBar.php
@@ -1,11 +1,13 @@
 <?php
+
 $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
+
 ?>
 
 <header>
 		<ul>
             <li> Welcome to Benj's Assignments homepage!</li>
             <br>
-			<li> We can access all his assignments here</li>
+			<li> We can access all his assignments below</li>
 		</ul>
 </header>
