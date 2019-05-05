ea600e6fd82d6d2f51d8d7adfb5e290d4f6c7601
bjaffray <jaf15001@byui.edu>
2019-05-04 23:08:15 -0600

    Update

--- web/index.php
+++ web/index.php
@@ -11,7 +11,99 @@
 
 <body>
   
-stuff
+<div id="table">
+  <table>
+        <tr>
+          <th> Week </th>
+          <th> Link </th>
+        </tr>
+
+        <tr>
+          <td> Week 1: </td>
+          <td> 
+          <a href="underCon.php"> Here </a> 
+          </td>
+        </tr>
+
+        <tr>
+          <td> Week 2: </td>
+          <td> 
+          <a href="underCon.php"> Here </a> 
+          </td>
+        </tr>
+
+        <tr>
+          <td> Week 3: </td>
+          <td> 
+            <a href="underCon.php"> Here </a> 
+          </td>
+        </tr>
+
+        <tr>
+          <td> Week 4: </td>
+          <td> 
+            <a href="underCon.php"> Here </a> 
+          </td>
+        </tr>
+
+        <tr>
+          <td> Week 5: </td>
+          <td> 
+            <a href="underCon.php"> Here </a> 
+          </td>
+        </tr>
+
+        <tr>
+          <td> Week 6: </td>
+          <td> 
+            <a href="underCon.php"> Here </a> 
+          </td>
+        </tr>
+
+        <tr>
+          <td> Week 7: </td>
+          <td> 
+            <a href="underCon.php"> Here </a>   
+          </td>
+        </tr>
+
+        <tr>
+          <td> Week 8: </td>
+          <td> 
+            <a href="assign8/assign.html"> Here </a> 
+          </td>
+        </tr>
+
+        <tr>
+          <td> Week 9: </td>
+          <td> 
+            <a href="directory.html"> Here </a> 
+          </td>
+        </tr>
+
+        <tr>
+          <td> Week 10: </td>
+          <td> 
+            <a href="underCon.php"> Here </a> 
+          </td>
+        </tr>
+
+        <tr>
+          <td> Week 11: </td>
+          <td> 
+            <a href="underCon.php"> Here </a> 
+          </td>
+        </tr>
+
+        <tr>
+          <td> Week 12: </td>
+          <td> 
+            <a href="underCon.php"> Here </a> 
+          </td>
+        </tr>
+
+  </table>
+</div>
 
 </body>
 
