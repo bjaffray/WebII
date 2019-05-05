a4cbf4bfe04a10fadbd1e4fd8253bf96ec5742aa
bjaffray <jaf15001@byui.edu>
2019-05-04 18:25:25 -0600

    update

--- web/index.php
+++ web/index.php
@@ -1,5 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <title>Personal Homepage</title>
+</head>
 
-  phpinfo();
 
-?>
+<body>
+  
+</body>
+
+
+</html>
