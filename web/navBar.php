64e2b80ec38ae5057ba6afcc5382dfdc843292fb
bjaffray <jaf15001@byui.edu>
2019-05-04 23:31:30 -0600

    edits

--- web/navBar.php
+++ web/navBar.php
@@ -2,11 +2,10 @@
 $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 ?>
 
-<h1>Make your own Mountain, Inc.</h1>
-
 <header>
 		<ul>
-			<li> Welcome to Benj's Assignments homepage!</li>
+            <li> Welcome to Benj's Assignments homepage!</li>
+            <br>
 			<li> We can access all his assignments here</li>
 		</ul>
 </header>
