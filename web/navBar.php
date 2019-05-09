a4c2c55f8d2ab2a9e9a75c6e42c6442453c2e4c2
bjaffray <jaf15001@byui.edu>
2019-05-08 18:30:20 -0600

    Heading/Background

--- web/navBar.php
+++ web/navBar.php
@@ -7,9 +7,9 @@ $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 <div class="w3-container w3-round-xlarge w3-black w3-margin-top w3-margin-left w3-margin-right w3-allerta">
 	<h1>
 		<ul>
-			<li> Welcome to Benj's Assignments homepage!</li>
+			<li> Welcome to Benj's Assignments homepage! </li>
 			<br>
-			<li> We can access all his assignments below</li>
+			<li> We can access all his assignments below </li>
 		</ul>
 </h1>
 </div>
--- web/week3/shop.php
+++ web/week3/shop.php
@@ -9,15 +9,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/style.css"> 
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Allerta+Stencil">
     <title>Shop</title>
 </head>
 
 
 
-<body>
+<body class="w3-allerta">
 
-
-    shop
+<div class="w3-container w3-dark-grey">
+    <img src="Dota2.png" class="w3-round w3-margin-left" alt="Dota2"> 
+    <p class="w3-xxxlarge">Welcome to the dota secret shop</p>
+</div> 
 
 
 
