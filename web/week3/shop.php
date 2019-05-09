cd13c938b4fe6ce7df1dae0f7ca45325f6bd050c
bjaffray <jaf15001@byui.edu>
2019-05-08 18:48:15 -0600

    font

--- web/week3/shop.php
+++ web/week3/shop.php
@@ -19,9 +19,9 @@
 
 <body>
 
-<div class="w3-container w3-dark-grey w3-allerta w3-round-xlarge w3-margin-top w3-margin-left w3-margin-right">
+<div class="w3-container w3-dark-grey w3-round-xlarge w3-margin-top w3-margin-left w3-margin-right">
     <img src="Dota2.png" class="w3-round w3-margin-left" alt="Dota2"> 
-    <span class="w3-xxxlarge w3-margin-top">Welcome to the dota secret shop</span>
+    <span class="w3-xxxlarge w3-margin-top w3-allerta">Welcome to the dota secret shop</span>
 </div> 
 
 
