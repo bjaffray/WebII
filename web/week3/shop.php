03dd834aa12e9c7839abd886bcd92449f595d019
bjaffray <jaf15001@byui.edu>
2019-05-09 14:01:27 -0600

    ClassStuff

--- web/week3/shop.php
+++ web/week3/shop.php
@@ -1,33 +1,10 @@
 <?php
     // Start the session
+    require("Item.php");
+
     session_start();
     $myfile = fopen("store.txt", "r") or die("Unable to open file!");
-
-    class Item {
-        // constructor
-        public function __construct($name, $price, $desc, $filep, $in_cart) {
-            $this->name = $name;
-            $this->price = $price;
-            $this->desc = $desc;
-            $this->filep = $filep;
-            $this->in_cart = $in_cart;
-        }
-
-
-        public function printInfo() {
-            echo "Name: " . $this->name . " | Price: " . $this->price . " | Description: " . $this->desc . " | File Path: " . $this->filep . "<br>";
-        }
-
-
-        public function addToCart() {
-            if ($this->in_cart)
-                $this->in_cart = false;
-            else
-                $this->in_cart = true;
-        }
-
-    }
-
+    
     $items = array();
 
     while(!feof($myfile)) {
