c388acdec8d4e2e760a183cd17d33493d6801d66
bjaffray <jaf15001@byui.edu>
2019-05-09 14:11:42 -0600

    edits

--- web/week3/cart.php
+++ web/week3/cart.php
@@ -34,9 +34,9 @@
 
 
 <br> <br>
-<div class="w3-bar w3-center">
-    <input type="button" name="tocheckout" value="Go to Checkout" class="w3-bar-item w3-button w3-xlarge w3-block w3-btn w3-dark-grey w3-round-large" onclick="window.location='checkout.php';">
-    <input type="button" name="toshop" value="Go back to Shop" class="w3-bar-item w3-button w3-xlarge w3-block w3-btn w3-dark-grey w3-round-large" onclick="window.location='shop.php';">
+<div class="w3-bar">
+    <button name="tocheckout" class="w3-bar-item w3-button w3-xlarge w3-block w3-btn w3-dark-grey w3-round-large" style="width:50%" onclick="window.location='checkout.php';">Go to Checkout</button>
+    <button name="toshop" class="w3-bar-item w3-button w3-xlarge w3-block w3-btn w3-dark-grey w3-round-large" style="width:50%" onclick="window.location='shop.php';">Go Back to Shop</button>
 </div> 
 
 </body>
--- web/week3/shop.php
+++ web/week3/shop.php
@@ -64,8 +64,7 @@ require("banner.php");
     </table>
 
     <br> <br>
-
-    <input type="button" name="tocart" value="Go to Cart" class="w3-button w3-xlarge w3-block w3-center w3-btn w3-dark-grey w3-round-large" onclick="window.location='cart.php';">
+    <button name="tocart" class="w3-bar-item w3-button w3-xlarge w3-block w3-btn w3-dark-grey w3-round-large" onclick="window.location='cart.php';">Go to Cart</button>
 </div>
 
 </body>
