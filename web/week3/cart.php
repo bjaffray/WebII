2e5c24701f8bbdc11d816181d17e52eea019ce22
bjaffray <jaf15001@byui.edu>
2019-05-09 14:07:34 -0600

    back and forward

--- web/week3/cart.php
+++ web/week3/cart.php
@@ -25,11 +25,19 @@
 <?php
     require("banner.php");
 ?>
+<br> <br>
 
 
 
 
 
+
+
+<br> <br>
+<div class="w3-bar">
+    <input type="button" name="tocheckout" value="Go to Checkout" class="w3-bar-item w3-button w3-xlarge w3-block w3-btn w3-dark-grey w3-round-large" onclick="window.location='checkout.php';">
+    <input type="button" name="toshop" value="Go back to Shop" class="w3-bar-item w3-button w3-xlarge w3-block w3-btn w3-dark-grey w3-round-large" onclick="window.location='shop.php';">
+</div> 
 
 </body>
 
