873e6d97c3f2396ca42ecde3465917b7cde2ba17
bjaffray <jaf15001@byui.edu>
2019-05-09 13:40:14 -0600

    size check

--- web/week3/cart.php
+++ web/week3/cart.php
@@ -26,11 +26,12 @@
 
 
 <div class="w3-container w3-dark-grey w3-round-xlarge w3-margin">
-    <?php    
-    foreach ($_SESSION['store_items'] as $value) {
-        echo "Item ";
-        $value->printInfo();
-    }
+    <?php   
+    echo "Size: " . sizeof($_SESSION['store_items']);
+    // foreach ($_SESSION['store_items'] as $value) {
+    //     echo "Item ";
+    //     $value->printInfo();
+    // }
     ?>
 </div>
 
