92c76db8d42f902a71b86623d75e92f4361fbf42
bjaffray <jaf15001@byui.edu>
2019-05-08 18:47:08 -0600

    stuff

--- web/week3/shop.php
+++ web/week3/shop.php
@@ -20,7 +20,7 @@
 <body>
 
 <div class="w3-container w3-dark-grey w3-allerta w3-round-xlarge w3-margin-top w3-margin-left w3-margin-right">
-    <img src="Dota2.png" class="w3-round w3-margin-left" alt="Dota2"> <p class="w3-xxxlarge">Welcome to the dota secret shop</p>
+    <img src="Dota2.png" class="w3-round w3-margin-left" alt="Dota2"> <span class="w3-xxxlarge">Welcome to the dota secret shop</span>
 </div> 
 
 
