789e89ae9b3ce46025467cef31868e328e723e07
bjaffray <jaf15001@byui.edu>
2019-05-08 18:41:00 -0600

    changes

--- web/week3/shop.php
+++ web/week3/shop.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/style.css"> 
+    <link rel="stylesheet" href="style.css"> 
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Allerta+Stencil">
     <title>Shop</title>
@@ -20,10 +20,11 @@
 <body>
 
 <div class="w3-container w3-dark-grey w3-allerta">
-    <img src="Dota2.png" class="w3-round w3-margin-left" alt="Dota2"> 
-    <div class="w3-xxxlarge">Welcome to the dota secret shop</div>
+    <img src="Dota2.png" class="w3-round w3-margin-left" alt="Dota2"> <u class="w3-xxxlarge">Welcome to the dota secret shop</u>
 </div> 
 
+
+text
 
 
 </body>
