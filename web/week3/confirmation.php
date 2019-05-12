45723b6ea5acc413fb0ee765bb816abc8a682493
bjaffray <jaf15001@byui.edu>
2019-05-11 22:17:50 -0600

    edits

--- web/week3/confirmation.php
+++ web/week3/confirmation.php
@@ -56,9 +56,9 @@
         }
     }
 
-    echo "For a total of " . $_SESSION['total_price'] . " gold will go to the address:<br>";
+    echo "<br>For a total of " . $_SESSION['total_price'] . " gold will go to the address:<br>";
     echo $_SESSION['street1'] . ", " . $_SESSION['street2'] . ", " . $_SESSION['city'] . ", " . $_SESSION['state'] . ", " . $_SESSION['zip'] . "<br><br><br>";
-    echo "Do you want to accept this order?<br><br><br><br>";
+    echo "<h1>Do you want to accept this order?</h1><br><br><br>";
 
     ?>
 
