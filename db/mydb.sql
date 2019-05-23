6e1529da5ed88c146b28828e15bae91bfef8721c
bjaffray <jaf15001@byui.edu>
2019-05-23 17:14:54 -0600

    updates

--- db/mydb.sql
+++ db/mydb.sql
@@ -1,67 +1,50 @@
 -- Table: public."User"
 
--- DROP TABLE public."User";
+ DROP TABLE public."User";
 
 CREATE TABLE public."User"
-(
-    "UserID" integer NOT NULL,
-    "FirstName" VARCHAR(30) NOT NULL,
-    "MiddelName" VARCHAR(30),
-    "LastName" VARCHAR(30) NOT NULL,
-    "Zip" integer NOT NULL,
-    CONSTRAINT "User_pkey" PRIMARY KEY ("UserID")
-)
-WITH (
-    OIDS = FALSE
-)
+("UserID"       INTEGER         NOT NULL
+,"FirstName"    VARCHAR(30)     NOT NULL
+,"MidName"      VARCHAR(30)
+,"LastName"     VARCHAR(30)     NOT NULL
+,"Zip"          INTEGER         NOT NULL
+,CONSTRAINT "user_pk_01" PRIMARY KEY ("UserID"))
+WITH (OIDS = FALSE)
 TABLESPACE pg_default;
 
 -- Table: public."Group"
 
--- DROP TABLE public."Group";
+ DROP TABLE public."Group";
 
 CREATE TABLE public."Group"
-(
-    "GroupID" integer NOT NULL,
-    "UserID" integer NOT NULL,
-    "Zip" integer NOT NULL,
-    "Classification" VARCHAR(30),
-    CONSTRAINT "Group_pkey" PRIMARY KEY ("GroupID"),
-    CONSTRAINT "Group_fk_01" FOREIGN KEY ("UserID")
-        REFERENCES public."User" ("UserID") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-WITH (
-    OIDS = FALSE
-)
+("GroupID"          INTEGER     NOT NULL
+,"UserID"           INTEGER     NOT NULL
+,"Zip"              INTEGER     NOT NULL
+,"Classification"   VARCHAR(30) NOT NULL
+,CONSTRAINT "group_pk_01" PRIMARY KEY ("GroupID")
+,CONSTRAINT "group_fk_01" FOREIGN KEY ("UserID") REFERENCES public."User" ("UserID") MATCH SIMPLE 
+    ON UPDATE NO ACTION ON DELETE NO ACTION)
+WITH (OIDS = FALSE)
 TABLESPACE pg_default;
 
 
 -- Table: public."Event"
 
--- DROP TABLE public."Event";
+ DROP TABLE public."Event";
 
 CREATE TABLE public."Event"
-(
-    "EventID" integer NOT NULL,
-    "GroupID" integer,
-    "UserID" integer,
-    "Zip" integer NOT NULL,
-    "Classification" VARCHAR(30),
-    CONSTRAINT "Event_pkey" PRIMARY KEY ("EventID"),
-    CONSTRAINT "Event_fk_01" FOREIGN KEY ("GroupID")
-        REFERENCES public."Group" ("GroupID") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT "Event_fk_02" FOREIGN KEY ("UserID")
-        REFERENCES public."User" ("UserID") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-WITH (
-    OIDS = FALSE
-)
+("EventID"          INTEGER     NOT NULL
+,"GroupID"          INTEGER
+,"UserID"           INTEGER     NOT NULL
+,"Zip"              INTEGER     NOT NULL
+,"Date"             DATE        NOT NULL
+,"Classification"   VARCHAR(30) NOT NULL
+,CONSTRAINT "event_pk_01" PRIMARY KEY ("EventID")
+,CONSTRAINT "event_fk_01" FOREIGN KEY ("GroupID") REFERENCES public."Group" ("GroupID") MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE NO ACTION
+,CONSTRAINT "event_fk_02" FOREIGN KEY ("UserID") REFERENCES public."User" ("UserID") MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE NO ACTION)
+WITH (OIDS = FALSE)
 TABLESPACE pg_default;
 
 
---
+++ web/week5/readDB.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css"> 
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+    <title>See Database information</title>
+</head>
+
+
+
+
+<body>
+    
+
+
+
+
+
+
+
+</body>
+</html>
