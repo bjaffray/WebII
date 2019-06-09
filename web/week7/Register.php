0dd4a276afc40484a81dbd68a43a80e8d8f19d62
bjaffray <jaf15001@byui.edu>
2019-06-09 10:23:43 -0600

    size

--- web/week7/Register.php
+++ web/week7/Register.php
@@ -44,7 +44,7 @@ if(isset($_POST['goHome'])) {
 
     <br> <br> <br> <br>
     <div class="w3-bar footer">
-        <input type="submit" name="goHome" style="width:50%"
+        <input type="submit" name="goHome" style="width:100%"
             class="w3-bar-item w3-button w3-xlarge w3-block w3-btn w3-dark-grey w3-round-large w3-border" value="Go to Homepage">
     </div>
 </div> 
