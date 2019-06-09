4de6e324af178dd00737892bfdb5495dd6bed5f1
bjaffray <jaf15001@byui.edu>
2019-06-08 21:37:35 -0600

    clarification

--- web/week7/findGorE.php
+++ web/week7/findGorE.php
@@ -23,7 +23,7 @@ if(isset($_POST['goHome'])) {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="styles.css"> 
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
-    <title>Looking For Group Finder</title>
+    <title>Group or Event finder</title>
 </head>
 
 
@@ -40,7 +40,7 @@ if(isset($_POST['goHome'])) {
 
     <div class="w3-container w3-dark-grey w3-round-xlarge w3-allerta w3-center w3-border">
 
-        <h1>Welcome to the main page for the LFG Finder</h1>
+        <h1>Welcome to the finder page for the LFG Finder</h1>
 
         <p>This is a is a description paragraph</p>
 
